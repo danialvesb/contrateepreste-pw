4be3e97ad8a2b9a8f7832c1f4ebec0dde212b2d2
daniel <daddani14al@gmail.com>
2020-01-11 17:12:23 -0300

    Inicio

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+  },
+
 }
 </script>
 
