e32b7747d71e26fff4af340d369b2c8c5a5b7173
Daniel <daniel@appfacilita.com>
2020-03-07 19:53:43 -0300

    image added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <b-container fluid class="m-0 p-0">
-    <b-card overlay img-src="https://picsum.photos/900/250/?image=3" img-alt="Card Image" text-variant="white" title="Contrate & Preste" sub-title="Painel de Gestão">
+    <b-card overlay  :img-src="require('../assets/home.jpg')" img-alt="Card Image" text-variant="white" title="Contrate & Online" sub-title="Painel de Gestão">
+      <b-img></b-img>
       <b-card-text>
 
       </b-card-text>
@@ -13,7 +14,8 @@ import  { BCard, BCardText } from 'bootstrap-vue'
 export default {
   components: {
     'b-card': BCard,
-    'b-card-text': BCardText
+    'b-card-text': BCardText,
+    
   }
 }
 </script>
